8c7dd1b366435ed1668d05c150d53e5dc99684ef
oliver0804 <icetzsr@gmail.com>
2024-08-26 12:27:05 +0800

    update pip 套件

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
+# 獲取腳本所在的目錄
+SCRIPT_DIR=$(dirname "$0")
+
+# 在腳本所在目錄下創建 PHOENIX2014T 目錄
+mkdir -p "$SCRIPT_DIR/PHOENIX2014T"
+
+# 切換到 PHOENIX2014T 目錄
+cd "$SCRIPT_DIR/PHOENIX2014T"
+
+# 下載檔案
 wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.train"
 wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.dev"
 wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.test"
+
